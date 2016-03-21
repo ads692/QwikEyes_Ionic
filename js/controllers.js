8098633d734e9f346a863c2dcb5eb6e9fdca1ec3
Aditya <aditya.murthy@asu.edu>
2016-03-20 20:59:18 -0700

    Initial template created with Ionic

---
+++ js/controllers.js
@@ -0,0 +1,34 @@
+angular.module('app.controllers', [])
+  
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('menuCtrl', function($scope) {
+
+})
+   
+.controller('pictureCaptureCtrl', function($scope) {
+
+})
+   
+.controller('videoCallCtrl', function($scope) {
+
+})
+   
+.controller('accountSettingsCtrl', function($scope) {
+
+})
+   
+.controller('deleteAccountCtrl', function($scope) {
+
+})
+   
+.controller('aboutQwikEyesCtrl', function($scope) {
+
+})
+ 
